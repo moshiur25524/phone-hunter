bb6e3de2ecb81580344e046a90fbb9dfcb4c536f
moshiur25524 <moshiur.rahman25524@gmail.com>
2022-03-02 05:47:16 +0600

    card added

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
 const searchButton = () => {
-    console.log('Button working');
+    const searchField = document.getElementById('search-field')
+    const searchText = searchField.value;
+    // console.log(searchText);
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayPhones(data.data))
+}
+
+const displayPhones = (phones) => {
+    // console.log(phones);
+    const cardContainer = document.getElementById('card-container')
+    phones.forEach(phone => {
+        console.log(phone);
+        const div =  document.createElement('div')
+        div.classList.add('col-lg-4')
+        div.classList.add('my-5')
+        div.innerHTML = `
+        <div class="card" style="width: 18rem;">
+        <img src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${phone.phone_name}</h5>
+          <h5 class="card-title">${phone.brand}</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+        `
+        cardContainer.appendChild(div)
+    });
 }

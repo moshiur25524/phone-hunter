0d8ab059bfeecae8d38dd11b985f48721ddea0e2
moshiur25524 <moshiur.rahman25524@gmail.com>
2022-03-02 06:47:53 +0600

    card without button done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const searchButton = () => {
     const searchField = document.getElementById('search-field')
     const searchText = searchField.value;
+    searchField.value = '';
     // console.log(searchText);
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
@@ -13,6 +14,7 @@ const displayPhones = (phones) => {
     const cardContainer = document.getElementById('card-container')
     phones.forEach(phone => {
         console.log(phone);
+        
         const div =  document.createElement('div')
         div.classList.add('col-lg-4')
         div.classList.add('my-5')
@@ -23,10 +25,14 @@ const displayPhones = (phones) => {
           <h5 class="card-title">${phone.phone_name}</h5>
           <h5 class="card-title">${phone.brand}</h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <button onclick="cardDetails('${phone.slug}')" class="btn btn-primary">details</button>
         </div>
       </div>
         `
         cardContainer.appendChild(div)
+        
     });
+}
+const cardDetails = (card) => {
+    console.log(card);
 }

b7ff9f55784452bc853cd97271dd03ac46a3e6ec
moshiur25524 <moshiur.rahman25524@gmail.com>
2022-03-02 12:10:22 +0600

    mostly error and phone Detail

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const searchButton = () => {
     const searchField = document.getElementById('search-field')
+    const error = document.getElementById('error')
     const searchText = searchField.value;
     searchField.value = '';
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    error.innerText = ''
+    if(searchText == 0){
+      error.innerText = 'Please Search any phone';
+    }
+    // else if(searchText !== 'iphone'|| searchText !== 'samsung' || searchText !== 'oppo'){
+    //   error.innerText = 'Your have to choose Iphone, Samsung, Oppo'
+    // }
+    else{
+      const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
     .then(res => res.json())
     .then(data => displayPhones(data.data))
+    }
+    
 }
 
 const displayPhones = (phones) => {
@@ -35,11 +46,32 @@ const displayPhones = (phones) => {
     });
     
 }
+ // phone Details....
 
 const loadPhoneDetail = id => {
     console.log(id);
     const url = `https://openapi.programming-hero.com/api/phone/${id}`
     fetch(url)
     .then(res => res.json())
-    .then(data =>console.log(data))
+    .then(data =>displayPhoneDetail(data.data))
+}
+
+const displayPhoneDetail = phone => {
+  console.log(phone);
+  const phoneDetail = document.getElementById('phone-details')
+  phoneDetail.innerHTML = '';
+  const div = document.createElement('div')
+  div.classList.add('card');
+  div.innerHTML = `
+  <img src="${phone.image}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${phone.slug}</h5>
+    <h5 class="card-title">${phone.releaseDate}</h5>
+    <h3 class="card-title">Basic Feature</h3>
+    <p class="card-text">Chip Set : ${phone.mainFeatures.chipSet}</p>
+    <p class="card-text">Display Size : ${phone.mainFeatures.displaySize}</p>
+    <p class="card-text">Memory : ${phone.mainFeatures.memory}</p>
+  </div>
+  `
+  phoneDetail.appendChild(div);
 }

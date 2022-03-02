2c7c79a81bf75ad3b9e7b7c7175a6b2c6ccb3e81
moshiur25524 <moshiur.rahman25524@gmail.com>
2022-03-02 09:43:55 +0600

    problem solving

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const searchButton = () => {
     const searchField = document.getElementById('search-field')
     const searchText = searchField.value;
     searchField.value = '';
-    // console.log(searchText);
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
     .then(res => res.json())
@@ -15,7 +14,7 @@ const displayPhones = (phones) => {
     const cardContainer = document.getElementById('card-container')
     cardContainer.innerHTML = '';
     first20phone.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
         
         const div =  document.createElement('div')
         div.classList.add('col-lg-4')
@@ -27,7 +26,7 @@ const displayPhones = (phones) => {
           <h5 class="card-title">${phone.phone_name}</h5>
           <h5 class="card-title">${phone.brand}</h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <button onclick="cardDetails('${phone.slug}')" class="btn btn-primary">details</button>
+          <button onclick="loadPhoneDetail('${phone.slug}')" class="btn btn-primary">details</button>
         </div>
       </div>
         `
@@ -37,6 +36,10 @@ const displayPhones = (phones) => {
     
 }
 
-const cardDetails = (card) => {
-    console.log(card);
+const loadPhoneDetail = id => {
+    console.log(id);
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>console.log(data))
 }
